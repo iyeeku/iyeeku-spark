3b7aa8df0164d4beffb08059dffd9fbe7d15f0f5
iyeeku <iyeeku@qq.com>
2019-05-21 09:39:09 +0800

    Initial commit
    Signed-off-by: iyeeku <iyeeku@qq.com>

--- iyeeku-spark-common/src/main/java/com/iyeeku/spark/common/ExecSql.java
+++ iyeeku-spark-common/src/main/java/com/iyeeku/spark/common/ExecSql.java
@@ -1,9 +1,15 @@
 package com.iyeeku.spark.common;
 
+import org.apache.spark.sql.SparkSession;
+
 public class ExecSql {
 
     public static void main(String[] args) {
-
+        SparkSession session = SparkSession.builder()
+                .appName("ExecSql")
+                .master("local[*]")
+                .enableHiveSupport()
+                .getOrCreate();
     }
 
 }
