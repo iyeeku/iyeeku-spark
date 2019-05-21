b93d87e534be266ba72e2745d2ef1cddef03a380
iyeeku <iyeeku@qq.com>
2019-05-21 13:45:24 +0800

    Initial commit
    Signed-off-by: iyeeku <iyeeku@qq.com>

--- iyeeku-spark-common/src/main/java/com/iyeeku/spark/common/ExecSql.java
+++ iyeeku-spark-common/src/main/java/com/iyeeku/spark/common/ExecSql.java
@@ -1,15 +1,65 @@
 package com.iyeeku.spark.common;
 
+import com.iyeeku.spark.util.SqlParseUtil;
+import org.apache.commons.cli.*;
+import org.apache.commons.io.FileUtils;
+import org.apache.spark.SparkContext;
 import org.apache.spark.sql.SparkSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ExecSql {
 
-    public static void main(String[] args) {
+    public static String sqlFileName = "";
+    public static Options opts = new Options();
+    public  static Logger logger = LoggerFactory.getLogger(ExecSql.class);
+
+    public static void printHelp(){
+        new HelpFormatter().printHelp("ExecSql" , opts);
+        System.exit(1);
+    }
+
+    public static void validateArgs(String[] args) throws ParseException{
+        opts.addOption("f" , true , "Run Sql File");
+
+        CommandLine commandLine = new GnuParser().parse(opts , args);
+        if (commandLine.hasOption("f")){
+            sqlFileName = commandLine.getOptionValue("f");
+        } else {
+            printHelp();
+        }
+
+        if (!FileUtils.getFile(sqlFileName).exists()){
+            System.err.println("Sql File:[" + sqlFileName + "] Not Found!");
+            System.exit(1);
+        }
+    }
+
+    public static void runSql(SparkSession session , String sqlFileName) throws IOException{
+        String allSql = SqlParseUtil.readLinesExceptBlank(new File(sqlFileName));
+        SqlParseUtil.runSqlSegment(session , allSql);
+    }
+
+    public static void main(String[] args) throws IOException {
+        try {
+            validateArgs(args);
+        }catch (ParseException pe){
+            pe.printStackTrace();
+        }
+
         SparkSession session = SparkSession.builder()
                 .appName("ExecSql")
                 .master("local[*]")
                 .enableHiveSupport()
                 .getOrCreate();
+
+        SparkContext context = session.sparkContext();
+        context.setLogLevel("WARN");
+        runSql(session , sqlFileName);
+        session.stop();
     }
 
 }
--- iyeeku-spark-common/src/main/java/com/iyeeku/spark/util/SqlParseUtil.java
+++ iyeeku-spark-common/src/main/java/com/iyeeku/spark/util/SqlParseUtil.java
@@ -1,4 +1,108 @@
 package com.iyeeku.spark.util;
 
+import com.iyeeku.spark.common.ExecSql;
+import org.apache.commons.io.FileUtils;
+import org.apache.spark.sql.SparkSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SqlParseUtil {
+
+    public static final String[] note = {"#","--"};
+    public static final String multnote = "/*";
+
+    public static int getLeftNoteLine(String line){
+        int[] _line_position_arr = new int[note.length];
+        int index = 0;
+        for (String eachnote : note){
+            int _p = line.indexOf(eachnote);
+            _line_position_arr[index] = _p;
+            index ++;
+        }
+
+        int line_position = -1;
+        for (int _line_position : _line_position_arr){
+            if (_line_position >= 0 && line_position < 0)
+                line_position = _line_position;
+            if (_line_position >=0 && _line_position < line_position)
+                line_position = _line_position;
+        }
+        return line_position;
+    }
+
+    /**
+     * 将sql中多余的空格转化为一个空格
+     * @param sqlString
+     * @return
+     */
+    public static String normalSql(String sqlString){
+        String[] strArray = sqlString.split("\\s+");
+        StringBuffer outString = new StringBuffer();
+        for (String word : strArray){
+            outString.append(word).append(" ");
+        }
+        return outString.toString().trim();
+    }
+
+    /**
+     * 分析sql得到sql数组
+     * @param sqlString
+     * @return
+     */
+    public static String[] spliteSql(String sqlString){
+        String[] sqlArray = sqlString.split(";");
+        List<String> list = new ArrayList<String>();
+        for (String sql : sqlArray){
+            String str = sql.trim();
+            if (str.length() > 0){
+                list.add(normalSql(str));
+            }
+        }
+        return list.toArray(new String[list.size()]);
+    }
+
+    public static void runSqlSegment(SparkSession session , String allString){
+
+        String[] sqlArray = spliteSql(allString);
+
+        Logger logger = LoggerFactory.getLogger(ExecSql.class);
+
+        for (String sql : sqlArray){
+            logger.warn("runSql:[ " + sql + " ]");
+            long start = System.currentTimeMillis();
+            session.sql(sql).show();
+            long end = System.currentTimeMillis();
+            logger.warn("runSql:[ " + (end - start) + " ] ms");
+        }
+
+    }
+
+
+    public static String readLinesExceptBlank(File filePath) throws IOException{
+
+        List<String> lines = FileUtils.readLines(filePath);
+        StringBuffer outString = new StringBuffer();
+
+        String multnote_flg = "0";
+        String return_str = "";
+        String line = "";
+        for (String _line : lines){
+            line = _line.trim();
+            if (line.length() == 0){
+                continue;
+            }
+
+            outString.append(line).append("\n");
+
+        }
+
+
+        return normalSql(outString.toString());
+    }
+
 }
