f4a7cd1a16293c3df51a4f00513b06796fe0450c
Stephen <iyeeku@qq.com>
2019-10-05 15:58:57 +0800

    Initial commit Signed-off-by: iyeeku <iyeeku@qq.com>
    Signed-off-by: Stephen <iyeeku@qq.com>

--- iyeeku-spark-common/src/main/java/com/iyeeku/spark/common/FixedFileToDataFrame.java
+++ iyeeku-spark-common/src/main/java/com/iyeeku/spark/common/FixedFileToDataFrame.java
@@ -8,8 +8,6 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SQLContext;
-import org.apache.spark.sql.types.DataTypes;
-import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 
 import java.io.File;
